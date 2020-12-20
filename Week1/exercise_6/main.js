88d351a3c2f637ff696f2f40d71e393c99952f91
Vaire444 <73930132+Vaire444@users.noreply.github.com>
2020-12-20 20:16:04 +0200

    Create main.js

---
+++ Week1/exercise_6/main.js
@@ -0,0 +1,31 @@
+let a = '1';
+let b = '12';
+let c = '123';
+let d = '1234';
+let e = '12345';
+let f = '123456';
+let g = '1234567';
+let h = '12345678';
+let i = '123456789';
+
+for (let j = 1; j <= 49; j++) {
+    if (j == 1 || j == 17 || j == 33 || j == 49) {
+        console.log(a);
+    } else if (j == 2 || j == 16 || j == 18 || j == 32 || j == 34 || j == 48) {
+        console.log(b);
+    } else if (j == 3 || j == 15 || j == 19 || j == 31 || j == 35 || j == 47) {
+        console.log(c);
+    } else if (j == 4 || j == 14 || j == 20 || j == 30 || j == 36 || j == 46) {
+        console.log(d);
+    } else if (j == 5 || j == 13 || j == 21 || j == 29 || j == 37 || j == 45) {
+        console.log(e);
+    } else if (j == 6 || j == 12 || j == 22 || j == 28 || j == 38 || j == 44) {
+        console.log(f);
+    } if (j == 7 || j == 11 || j == 23 || j == 27 || j == 39 || j == 43) {
+        console.log(g);
+    } else if (j == 8 || j == 10 || j == 24 || j == 26 || j == 40 || j == 42) {
+        console.log(h);
+    } else if (j == 9 || j == 25 || j == 41 || j == 41) {
+        console.log (i);
+    }
+}
